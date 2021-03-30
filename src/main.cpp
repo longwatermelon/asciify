afd4da4b246865460930786c9e24fe62c1bfb7f9
longwatermelon <qianhansenb@gmail.com>
2021-03-30 06:43:11 -0700

    safer argument parsing and help

--- src/main.cpp
+++ src/main.cpp
@@ -39,42 +39,98 @@ void generate_ascii(const std::vector<int>& intensities, int xmax, int ymax, boo
 }
 
 
+std::string next_arg(int argc, char** argv, int i)
+{
+	if (++i >= argc)
+	{
+		std::cout << argv[i - 1] << ": missing required argument\n";
+		exit(1);
+	}
+
+	return argv[i];
+}
+
+
+void help()
+{
+	std::cout << "Turn images into ascii\n";
+	std::cout << "-f <filename>: write ascii output to a file\n";
+	std::cout << "-s <size>: set image size to <size>x<size>\n";
+	std::cout << "-w  <width>: set image width to <width>\n";
+	std::cout << "-h <height>: set image height to <height>\n";
+	std::cout << "-rs <resize percent>: resize image to percentage of its original size. -rs 75 resizes the image to 75%\n";
+}
+
+
 int main(int argc, char** argv)
 {
+	if (argc == 1 || strcmp(argv[1], "--help") == 0)
+	{
+		help();
+		exit(0);
+	}
+
 	std::string path = argv[1];
+	
+
+	std::ifstream file(path);
+	if (!file)
+	{
+		std::cout << "file doesn't exist: " << path << "\n";
+		exit(1);
+	}
 
 	bool write_to_file = false;
 	std::string filename;
 
-	int img_w = 44;
-	int img_h = 44;
+	cv::Mat imgtemp = cv::imread(path);
+
+	int img_w = imgtemp.cols;
+	int img_h = imgtemp.rows;
 
 	for (int i = 0; i < argc; ++i)
 	{
 		if (strcmp(argv[i], "-f") == 0)
 		{
 			write_to_file = true;
-			filename = argv[++i];
+			filename = next_arg(argc, argv, i);
 		}
 
 		if (strcmp(argv[i], "-s") == 0)
 		{
-			std::stringstream(argv[++i]) >> img_w;
+			std::stringstream(next_arg(argc, argv, i)) >> img_w;
 			img_h = img_w;
 		}
 
 		if (strcmp(argv[i], "-w") == 0)
-			std::stringstream(argv[++i]) >> img_w;
+			std::stringstream(next_arg(argc, argv, i)) >> img_w;
 
 		if (strcmp(argv[i], "-h") == 0)
-			std::stringstream(argv[++i]) >> img_h;
+			std::stringstream(next_arg(argc, argv, i)) >> img_h;
+
+		if (strcmp(argv[i], "-rs") == 0)
+		{
+			float resize_percent;
+			std::stringstream(next_arg(argc, argv, i)) >> resize_percent;
+
+			if (resize_percent == 0)
+			{
+				std::cout << "can't resize to 0%\n";
+				exit(0);
+			}
+
+			resize_percent /= 100.f;
+
+			img_w *= resize_percent;
+			img_h *= resize_percent;
+		}
 	}
 
-	cv::Mat imgtemp = cv::imread(path);
+	img_h /= 2;
+
 	cv::Mat img;
 
-	cv::resize(imgtemp, img, cv::Size(img_w, img_h));
-	
+	cv::resize(imgtemp, img, cv::Size(img_w, img_h));	
 
 	std::vector<int> intensities;
 
@@ -97,6 +153,11 @@ int main(int argc, char** argv)
 	}
 
 	generate_ascii(intensities, img.cols, img.rows, write_to_file, filename);
+
+	if ((img.cols >= 200 || img.rows >= 200) && !write_to_file)
+	{
+		std::cout << "ascii output is very large, use -f to write the output to a file. run asciify --help for more information\n";
+	}
 
 	return 0;
 }
